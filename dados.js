fa539e05ad5c007b96a7c0844bfb7f22d14bffa5
Bragax23 <161178637+Bragax23@users.noreply.github.com>
2025-03-25 22:57:58 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {   nome : "Bicicleta 29 GTS M1 Freio Hidráulico 27v TSI9 c/ Trava Ride New",
+        descricao : "Bicicleta GTSM1 é ideal para quem quer começar no Mountain Bike, Trilhas leves e percursos longos, ON ou OFF-Road. São 3 tipos de tamanhos de quadro, o peso total da bike é de 14.4 kg montada. A Bike é direto da Fábrica Oficial com mais de 28 anos no mercado e garantia exclusiva com suporte diferenciado. Suporta até 110 kg",
+        link : "https://www.lojagtsm1.com.br/bicicletas/aro-29/bicicleta-gts-aro-29-freio-a-disco-hidraulico-cambio-gtsm1-tsi9-27-marchas-e-amortecedor-gts-m1-ride-new-2022"
+    },
+    {
+        nome : "Bicicleta aro 26",
+        descricao : "",
+        link : "https://www.apple.com/iphone"
+    },
+    {
+        nome : "Bicicleta GTSM1 MTB20 Câmbio Shimano 7v Freio a Disco",
+        descricao : "Rebeldia e jovialidade para vencer os desafios: esta é a fórmula da nova bicicleta GTS M1 MTB20 7v shimano, ideal para a galerinha iniciante no mountain bike. O câmbio shimano de 7 velocidades garante a propulsão, enquanto o freio a disco e os pneus mistos paco tires sri-56 conferem frenagem precisa e muita segurança. A bicicleta GTS M1 MTB20 7v shimano é pura diversão, é puro MTB, garantida por quem tem mais de 30 anos de presença no mercado brasileiro de bicicletas. Peso de 11 kg suporta até 80 kg.",
+        link : "montagem-gtsm1-mtb20-7v-cambio-shimano-freio-disco?variant_id=16441https://www.lojagtsm1.com.br/componentes/kit-"
+    },
+    {
+        nome : "Camiseta Ciclista GTSM1 Manga Curta Sportswear",
+        descricao : "Tecnodry, tecido de rápida absorção, proporcionando equilíbrio térmico, dando maior rendimento a prática esportiva. Na exposição ao sol, proporciona proteção a sua saúde. Filtra os raios UVA e UVB afastando os efeitos nocivos da radiação solar que causam manchas, envelhecimento precoce, queimadura e câncer de pele. ",
+        link : "https://www.lojagtsm1.com.br/acessorios/roupas/camiseta-sportswear-gtsm1-manga-curta-2021?variant_id=9349"
+    },
+    {
+        nome : "Bicicleta aro 20",
+        descricao : "O MacBook Air é um notebook ultraleve da Apple, conhecido por sua portabilidade, design fino e desempenho eficiente, ideal para tarefas do dia a dia e mobilidade.",
+        link : "https://www.apple.com/macbook-air"
+    }
+];
